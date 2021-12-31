17271e8d6a16293fcbdd78847aab9e5e923f3e95
Zomievey <haylee_Hunter@aol.com>
2021-12-31 15:28:03 -0600

    remain edit

--- src/App.js
+++ src/App.js
@@ -66,7 +66,7 @@ function App() {
             </button>
           </div>
           <h1>
-            {todos.filter((todo) => !todo.complete).length}  items remaining
+            {todos.filter((todo) => !todo.complete).length}  items remain 
           </h1>
         </div>
       </div>
